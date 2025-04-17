bb6ca7774fa9237af2e6f701db004f669bd95e12
Chanikya <chanikya1097@gmail.com>
2025-04-17 16:53:53 -0400

    Initial commit

---
+++ src/test/java/com/jenkins/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
